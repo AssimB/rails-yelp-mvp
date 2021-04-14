c836f90aae828d89af2d265d9c9f8c45d853f690
Assim <bakkarassim@gmail.com>
2021-04-14 03:05:10 +0200

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'reviews/content'
+  get 'reviews/rating'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  resources :restaurants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+wok = { name: 'Wok', address: '7 Boundary St, marseille E2 7JE', category: 'chinese' },
+chez_luigi = { name: 'Chez Luigi', address: '89 Boundary St, Venise E2 7JE', category: 'italian' },
+little_tokyo = { name: 'Little Tokyo', address: '12 Boundary St, Tokyo E2 7JE', category: 'japanese' },
+bruxellois = { name: 'Bruxellois', address: '5 Boundary St, Bruxelle E2 7JE', category: 'belgian' },
+chez_pierro = { name: 'Chez Pierro', address: '36 Boundary St, Paris E2 7JE', category: 'french' }
+
+[ wok, chez_luigi, little_tokyo, bruxellois, chez_pierro ].each do |attributes|
+	restaurant = Restaurant.create!(attributes)
+	puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
